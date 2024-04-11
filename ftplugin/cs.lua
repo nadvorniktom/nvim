e57cf490d9c0d601b0614498b436d9c3fceb2519
nadvorniktom <145836150+nadvorniktom@users.noreply.github.com>
2024-04-11 16:38:41 +0200

    Delete ftplugin directory

--- ftplugin/cs.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '<F4>', ':split<CR> | :res 16 | :term<CR> | idotnet build<CR><C-\\><C-n>G', { silent = true })
-vim.keymap.set('n', '<F5>', ':split<CR> | :res 5 | :term<CR> | idotnet run<CR><C-\\><C-n>G', { silent = true })
-vim.keymap.set('n', '<F6>', ':split<CR> | :res 5 | :term<CR> | idotnet watch<CR><C-\\><C-n>G', { silent = true })
